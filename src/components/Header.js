8f38f02dfc442c6c30f8d04a762ac606f999b256
Emily Scudero <emily@MSI.COM>
2021-02-02 21:26:07 -0500

    update folders

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+// import react from 'react'
+// only need to import if using class components
+
+const Header = () => {
+    return (
+        <h5>My first functional component</h5>
+    )
+};
+
+export default Header;
